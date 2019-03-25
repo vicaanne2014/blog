cfd4e325cbd6867216f207ef0efcd36415fe9175
Vincent Edy Hartono <ehartono2009@gmail.com>
2019-03-26 05:13:23 +0700

    Extend the form functionality to be able to update question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -29,7 +29,12 @@
                                 </div>
                             </div>
                             <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}"> {{ $question->title }}</a></h3>
+                                <div class="d-flex align-item-center">
+                                    <h3 class="mt-0"><a href="{{ $question->url }}"> {{ $question->title }}</a></h3>
+                                    <div class="ml-auto">
+                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    </div>
+                                </div>
                                 <p class="lead">
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                     <small class="class-muted">{{ $question->created_date }}</small>
