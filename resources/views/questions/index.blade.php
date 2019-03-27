49514b15f4d6a1cecd3246691ff733837deadacc
Vincent Edy Hartono <ehartono2009@gmail.com>
2019-03-27 11:09:38 +0700

    Implement authorization using Policy

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,12 +32,16 @@
                                 <div class="d-flex align-item-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}"> {{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-                                        </form>
+                                        @can('update', $question)
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endcan
+                                        @can('delete', $question)
+                                            <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                                            </form>
+                                        @endcan
                                     </div>
                                 </div>
                                 <p class="lead">
